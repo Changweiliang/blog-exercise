80df83a2f71b1d9463568aafcdfc71100b356dff
Changwei Liang <changwei777@gmail.com>
2017-09-19 23:31:11 -0700

    post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+
+app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.HomePage.as_view(), name='homepage'),
-
+    url(r'^(?P<pk>\d+)/detail/$', views.blog_detail, name='blog_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from . import models
 
@@ -8,3 +8,15 @@ class HomePage(ListView):
     template_name = 'blog/homepage.html'
     paginate_by = 10
     context_object_name = 'blog_list'
+
+    def get_queryset(self):
+        return models.MyPost.objects.all().order_by('-published_time')
+
+
+def blog_detail(request, pk):
+    blog = get_object_or_404(models.MyPost, pk=pk)
+
+    context = {
+        'blog': blog
+    }
+    return render(request, 'blog/blog_detail.html', context)
