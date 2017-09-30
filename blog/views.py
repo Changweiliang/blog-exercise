8eb319cda22045fb7ee253b7be5539cd9fe2bb72
Changwei Liang <changwei777@gmail.com>
2017-09-30 10:25:56 -0700

    utils module added

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from . import models
 from .forms.blog_form import BlogForm
 from django.template.context import RequestContext
 from my_auth.models import Permission
+from .utils import my_paginator_style
 
 
 class HomePage(ListView):
@@ -17,26 +18,11 @@ class HomePage(ListView):
         return models.MyPost.objects.all().order_by('-published_time')
 
     def get_context_data(self, **kwargs):
-        pages_to_show_in_one_series = 5
         context = super(ListView, self).get_context_data(**kwargs)
         queryset = self.get_queryset()
-        page_size = self.get_paginate_by(queryset)
-        paginator, page, queryset, is_paginated = self.paginate_queryset(queryset, page_size)
-        # if there are multiple_page_series_to_show,
-        # the way to show page is different in template
-        # otherwise the page list is simple
-        context['multiple_page_series_to_show'] = 0
-
-        if paginator.num_pages > pages_to_show_in_one_series:
-            context['multiple_page_series_to_show'] = 1
-            context['page_number_to_be_show'] = [i + page.number - pages_to_show_in_one_series // 2
-                                                 for i in range(pages_to_show_in_one_series)]
-            if page.number < pages_to_show_in_one_series//2:
-                context['page_number_to_be_show'] = [i+1 for i in
-                                                     range(pages_to_show_in_one_series)]
-            if page.number + pages_to_show_in_one_series//2 >= paginator.num_pages:
-                context['page_number_to_be_show'] = [i+paginator.num_pages-pages_to_show_in_one_series+1
-                                                     for i in range(pages_to_show_in_one_series)]
+        paginator, page, queryset, is_paginated = self.paginate_queryset(queryset, self.get_paginate_by(queryset))
+        # add the page numbers to be shown in homepage to context
+        context = my_paginator_style(paginator, page, context, pages_num_2b_shown=5)
         return context
 
 
@@ -46,7 +32,6 @@ def blog_detail(request, pk):
     context = {
         'blog': blog
     }
-
     return render(request, template_name, context)
 
 
@@ -65,27 +50,28 @@ def edit_blog(request,pk):
 
     render(request, template_name, context)
 
+
 @auth_decorators.login_required()
 def create_blog(request):
     template_name = 'blog/create_blog.html'
     context = {}
     if request.method == 'POST':
-        print('if')
+        # print('if')
         new_blog_form = BlogForm(request.POST)
-        print(new_blog_form.is_valid())
+        # print(new_blog_form.is_valid())
         if new_blog_form.is_valid():
             new_blog_cd = new_blog_form.cleaned_data
-            print(request.user)
+            # print(request.user)
             new_blog = models.MyPost.objects.create(author=request.user,
                 title=new_blog_cd['title'], tags=new_blog_cd['tags'],
                 content=new_blog_cd['content'],published_time=new_blog_cd['published_time'],
                 created_time=new_blog_cd['created_time']
             )
-            print(new_blog)
+            #print(new_blog)
             new_blog.save()
             return HttpResponseRedirect(reverse('blog:blog_detail', args=(new_blog.id,)))
     else:
-        print('else')
+        #print('else')
         new_blog_form = BlogForm()
         context['blog_form'] = new_blog_form
         return render(request,template_name, context)
